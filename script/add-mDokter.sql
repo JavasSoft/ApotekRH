38fa91a2f2da39111bfe7378e2c50d80dd358cf3
HasanBasri013 <135586019+HasanBasri013@users.noreply.github.com>
2025-11-05 15:44:22 +0700

    sql add
    mdokter

---
+++ script/add-mDokter.sql
@@ -0,0 +1,18 @@
+CREATE TABLE `mdokter` (
+	`IDDokter` INT(11) NOT NULL AUTO_INCREMENT,
+	`Kode` VARCHAR(50) NULL DEFAULT NULL COLLATE 'latin1_swedish_ci',
+	`Nama` VARCHAR(50) NULL DEFAULT NULL COLLATE 'latin1_swedish_ci',
+	`Email` VARCHAR(50) NULL DEFAULT NULL COLLATE 'latin1_swedish_ci',
+	`Alamat` VARCHAR(50) NULL DEFAULT NULL COLLATE 'latin1_swedish_ci',
+	`Telepon` VARCHAR(50) NULL DEFAULT NULL COLLATE 'latin1_swedish_ci',
+	`Kota` VARCHAR(50) NULL DEFAULT NULL COLLATE 'latin1_swedish_ci',
+	`Bank` VARCHAR(50) NULL DEFAULT NULL COLLATE 'latin1_swedish_ci',
+	`NomorRekening` VARCHAR(50) NULL DEFAULT NULL COLLATE 'latin1_swedish_ci',
+	`NamaRekening` VARCHAR(50) NULL DEFAULT NULL COLLATE 'latin1_swedish_ci',
+	`Aktif` TINYINT(4) NULL DEFAULT NULL,
+	PRIMARY KEY (`IDDokter`) USING BTREE,
+	UNIQUE INDEX `Kode` (`Kode`) USING BTREE
+)
+COLLATE='latin1_swedish_ci'
+ENGINE=InnoDB
+;
