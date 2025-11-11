5c19bbb21822cf24ab10fea13049f8fb927b4053
lukmqn <lukmanhakim30210@gmail.com>
2025-11-11 11:25:32 +0700

    up
    mitem

--- script/Add-mitem.sql
+++ script/Add-mitem.sql
@@ -1,13 +1,14 @@
-CREATE TABLE `mkategori` (
-	`IDKategori` INT NOT NULL AUTO_INCREMENT,
-	`Kode` VARCHAR(10) NULL DEFAULT NULL COLLATE 'latin1_swedish_ci',
-	`Nama` VARCHAR(50) NULL DEFAULT NULL COLLATE 'latin1_swedish_ci',
-	`Deskripsi` TEXT NULL DEFAULT NULL COLLATE 'latin1_swedish_ci',
-	`IsAktif` ENUM('Ya','Tidak') NULL DEFAULT NULL COLLATE 'latin1_swedish_ci',
-	PRIMARY KEY (`IDKategori`) USING BTREE,
+CREATE TABLE `mitem` (
+	`IDItem` INT NOT NULL AUTO_INCREMENT,
+	`Kode` VARCHAR(100) NOT NULL DEFAULT '0' COLLATE 'utf8mb4_0900_ai_ci',
+	`Nama` VARCHAR(100) NOT NULL DEFAULT '0' COLLATE 'utf8mb4_0900_ai_ci',
+	`Kategori` ENUM('Obat Bebas','Obat Prekursor','Obat Keras','Alat Kesehatan','Vitamin') NOT NULL DEFAULT 'Obat Bebas' COLLATE 'utf8mb4_0900_ai_ci',
+	`HargaBeli` DOUBLE NOT NULL DEFAULT '0',
+	`Aktif` TINYINT NOT NULL DEFAULT '0',
+	PRIMARY KEY (`IDItem`) USING BTREE,
 	UNIQUE INDEX `Kode` (`Kode`) USING BTREE
 )
-COLLATE='latin1_swedish_ci'
+COLLATE='utf8mb4_0900_ai_ci'
 ENGINE=InnoDB
-AUTO_INCREMENT=6
+AUTO_INCREMENT=12303
 ;
