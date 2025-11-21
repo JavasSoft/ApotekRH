60b243e140e9e4f247b14606b0c5138643cea706
HasanBasri013 <135586019+HasanBasri013@users.noreply.github.com>
2025-11-22 02:22:50 +0700

    ReturH dan RetruD sekalianJurnalitem

---
+++ src/model/Treturd.java
@@ -0,0 +1,37 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package model;
+
+public class Treturd {
+    private int idReturD;
+    private int idReturH;
+    private int idItem;
+    private double qty;
+    private double harga;
+    private double total;
+    private String satuan;
+
+    // getter & setter
+    public int getIdReturD() { return idReturD; }
+    public void setIdReturD(int idReturD) { this.idReturD = idReturD; }
+
+    public int getIdReturH() { return idReturH; }
+    public void setIdReturH(int idReturH) { this.idReturH = idReturH; }
+
+    public int getIdItem() { return idItem; }
+    public void setIdItem(int idItem) { this.idItem = idItem; }
+
+    public double getQty() { return qty; }
+    public void setQty(double qty) { this.qty = qty; }
+
+    public double getHarga() { return harga; }
+    public void setHarga(double harga) { this.harga = harga; }
+
+    public double getTotal() { return total; }
+    public void setTotal(double total) { this.total = total; }
+
+    public String getSatuan() { return satuan; }
+    public void setSatuan(String satuan) { this.satuan = satuan; }
+}
